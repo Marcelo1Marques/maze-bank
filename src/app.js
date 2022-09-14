01b74c23f34a1ce0243dfb098ba1f89c030e98aa
Marcelo1Marques <marcelo99david@gmail.com>
2022-09-14 08:49:09 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import "./assets/css/styles.css";
+import "@fortawesome/fontawesome-free/js/all";
+
+import Menu from "./components/menu";
+Menu();
+
+import darkMode from "./components/dark-mode";
+darkMode();
+
+let body = document.querySelector("body");
