e6d6cb74e0a1d97936a65041d1157c856230af40
Marcelo1Marques <marcelo99david@gmail.com>
2022-09-14 08:51:39 +0200

    update

--- src/app.js
+++ src/app.js
@@ -4,7 +4,4 @@ import "@fortawesome/fontawesome-free/js/all";
 import Menu from "./components/menu";
 Menu();
 
-import darkMode from "./components/dark-mode";
-darkMode();
-
 let body = document.querySelector("body");
