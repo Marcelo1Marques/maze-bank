fca7fd7d669f0f76d8ed991aacbfd1d81b3bd189
Marcelo1Marques <marcelo99david@gmail.com>
2022-09-29 15:39:01 +0200

    update

--- src/components/form-send.js
+++ src/components/form-send.js
@@ -2,7 +2,11 @@ const sendBtn = document.getElementById("send-btn");
 const nameInput = document.getElementById("name");
 const numberInput = document.getElementById("number");
 const emailInput = document.getElementById("email");
-const balanceCurr = document.getElementById("balance-curr")
+const balanceCurr = document.getElementById("balance-curr");
+const formName = document.getElementById("form-name");
+const formNumber = document.getElementById("form-number");
+const formEmail = document.getElementById("form-email");
+const formBalance = document.getElementById("form-balance");
 const error = document.getElementById("error");
 
 const send = () => {
@@ -11,7 +15,10 @@ const send = () => {
             error.textContent = "Please fill all fields"
             error.style.color = "red"
         } else {
-            
+            formName.textContent = nameInput.value
+            formNumber.textContent = numberInput.value
+            formEmail.textContent = emailInput.value
+            formBalance.textContent = balanceCurr.value + "€"
         }
     })
 };
