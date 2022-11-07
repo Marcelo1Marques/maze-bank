6209aa9fe71e7a87a4a5ab7479e5f873793f07d2
Marcelo1Marques <marcelo99david@gmail.com>
2022-11-07 16:40:11 +0100

    transfer form

--- src/components/transfer.js
+++ src/components/transfer.js
@@ -1,13 +1,14 @@
 const nameBeneficiary = document.getElementById("name-sent");
 const beneficiary = document.getElementById("transfer-name")
 const amountSent = document.getElementById("money-sent");
-const amount = document.getElementById("transfer-amount")
+const amount = document.getElementById("transfer-amount");
+const rib = document.getElementById("rib");
 const transferBtn = document.getElementById("transfer-button");
 const error = document.getElementById("error-transfer");
 
 const transfer = () => {
     transferBtn.addEventListener("click", () => {
-        if (!beneficiary.value || !amount.value) {
+        if (!beneficiary.value || !amount.value || !rib.value) {
             error.textContent = "Please fill all required fields"
             error.style.color = "red"
         } else {
